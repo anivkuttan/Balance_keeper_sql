b79d6e91de7cb3e7f5115e28839bcd9b26d00996
anivkuttan <anivkutta196@gmail.com>
2022-07-09 22:23:32 +0530

    thired commit

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -48,33 +48,96 @@ class _HomePageState extends State<HomePage> {
       child: Padding(
         padding: const EdgeInsets.all(5.0),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
             Container(
-              margin: const EdgeInsets.only(top: 20),
-              height: 200,
-              width: double.maxFinite,
-              decoration: BoxDecoration(
-                color: Colors.green,
-                borderRadius: BorderRadius.circular(23),
-              ),
-            ),
-            const SizedBox(height: 15),
+                margin: const EdgeInsets.only(top: 20),
+                height: 200,
+                width: double.maxFinite,
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.circular(23),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        const Text(
+                          'Anikuttan',
+                          style: TextStyle(fontSize: 30),
+                        ),
+                        const SizedBox(
+                          height: 30,
+                        ),
+                        const Text(
+                          'TotalAmount',
+                          style: TextStyle(fontSize: 27),
+                        ),
+                        Row(
+                          children: [
+                            Container(height: 40, width: 60, color: Colors.pinkAccent),
+                            const SizedBox(width: 20),
+                            Container(height: 40, width: 60, color: Colors.purple),
+                          ],
+                        ),
+                      ],
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: CircleAvatar(
+                        backgroundColor: Colors.white,
+                        radius: 40,
+                      ),
+                    )
+                  ],
+                )),
+            const SizedBox(height: 30),
             SizedBox(
               height: 60,
               width: double.maxFinite,
               child: ListView.separated(
                   scrollDirection: Axis.horizontal,
+                  physics: const BouncingScrollPhysics(),
                   itemBuilder: (context, index) {
                     return const CircleAvatar(
-                      backgroundColor: Colors.green,
-                      radius: 30,
+                      backgroundColor: Colors.red,
+                      radius: 35,
                     );
                   },
                   separatorBuilder: (context, index) {
                     return const SizedBox(width: 10);
                   },
                   itemCount: item.length),
-            )
+            ),
+            const SizedBox(height: 30),
+            SizedBox(
+              height: 60,
+              width: double.maxFinite,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text('Add Amount To all Person'),
+              ),
+            ),
+            const SizedBox(height: 30),
+            SizedBox(
+              height: 60,
+              width: double.maxFinite,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text('Add Amount To Some Person'),
+              ),
+            ),
+            const SizedBox(height: 30),
+            SizedBox(
+              height: 60,
+              width: double.maxFinite,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text('View all Person'),
+              ),
+            ),
           ],
         ),
       ),
